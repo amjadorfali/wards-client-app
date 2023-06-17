b92f9a7fdb5c4171485a98f77b98bae5ff79183f
Amjad Orfali <amjadorfali3@gmail.com>
2023-06-17 16:04:55 +0300

    Theming fixes

--- src/modules/Pages/LandingPage/sections/CodeSection.tsx
+++ src/modules/Pages/LandingPage/sections/CodeSection.tsx
@@ -20,55 +20,45 @@ const staggerAnimation = stagger(0.3);
 
 const animationSequence = Object.keys(elements)
 	.filter((id) => id != elements.codeBlock)
-	.map((id) => [`#${id}`, { opacity: 1, scale: [0.5, 1] }, { duration: 0.5, delay: staggerAnimation, at: '-0.2' }]);
+	.map((id) => [`#${id}`, { y: [40, 0], opacity: 1 }, { duration: 0.5, delay: staggerAnimation, at: '-0.2' }]);
 
 const CodeSection: React.FC = () => {
 	const theme = useTheme();
 	const [scope, animate] = useAnimate<HTMLDivElement>();
-	const inView = useInView(scope, { once: true, margin: '0px 0px -30% 0px' });
+	const inView = useInView(scope, { once: true, margin: '0px 0px -22% 0px' });
 
 	useEffect(() => {
 		if (inView) {
 			animate([...animationSequence], {});
-			animate(`#${elements.codeBlock}`, { opacity: 1, scale: [0.5, 1] }, { duration: 0.5 });
+			animate(`#${elements.codeBlock}`, { y: [40, 0], opacity: 1 }, { duration: 0.5 });
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [inView]);
 
 	return (
-		<Grid
-			container
-			justifyContent={'center'}
-			gap={10}
-			alignContent={'flex-end'}
-			pt={20}
-			sx={{
-				background: theme.palette.customBg.tertiaryGradient
-			}}
-			ref={scope}
-		>
-			<Grid item xs={11} sm={5} lg={3} container sx={{}} gap={3}>
-				<Typography sx={{ opacity: 0 }} id={elements.title} variant="h2" color={'text.secondary'}>
+		<Grid container justifyContent={'center'} gap={10} alignContent={'flex-end'} pt={20} ref={scope}>
+			<Grid item xs={11} sm={5} lg={3} container gap={3} alignContent={'flex-end'}>
+				<Typography sx={{ opacity: 0 }} id={elements.title} variant="h2">
 					{content.title.map((text) =>
 						typeof text === 'string' ? (
 							<React.Fragment key={text}>{text}</React.Fragment>
 						) : (
-							<Typography key={text.special} variant="body1" sx={{ ...theme.typography.h2 }} display={'inline'} color={'tertiary'}>
+							<Typography key={text.special} sx={{ ...theme.typography.h2 }} display={'inline'} color={'primary.light'}>
 								{' '}
 								{text.special}
 							</Typography>
 						)
 					)}
 				</Typography>
-				<Typography sx={{ opacity: 0 }} id={elements.subtitle} variant="subtitle1">
+				<Typography sx={{ opacity: 0 }} id={elements.subtitle}>
 					{content.subtitle}
 				</Typography>
 
-				<Grid item xs={11} sx={{ opacity: 0 }} id={elements.bashBlock} md={12}>
+				<Grid sx={{ opacity: 0 }} item xs={11} id={elements.bashBlock} md={12}>
 					<DemoBashBlock />
 				</Grid>
 			</Grid>
-			<Grid item xs={11} sx={{ opacity: 0 }} id={elements.codeBlock} sm={5} lg={3}>
+			<Grid sx={{ opacity: 0 }} item xs={11} id={elements.codeBlock} sm={5} lg={3}>
 				<DemoCodeBlock />
 			</Grid>
 		</Grid>
